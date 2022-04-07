28c8d88b962a75d07b137135378b3edd1dabd0bf
Arkajit Choudhury <Arkajit_Choudhury@epam.com>
2022-04-07 10:58:24 +0530

    initial commit

---
+++ fetchingFromAPI.js
@@ -0,0 +1,47 @@
+async function fetchingFromAPI(){
+    const searchKey = text;
+    console.log("This is searchKey file "+searchKey);
+
+    const APIKEY = "AIzaSyDsXJuqQWQiYb3BWrtxNVxaXaPmjhi7xAo";
+    const url = "https://www.googleapis.com/youtube/v3/search?key="+APIKEY+
+    "&type=video&part=snippet&maxResults=100&q="+searchKey;
+    var data;
+    var video = '';
+    await fetch(url)
+        .then(response => response.json())
+        .then(d => {data = d})
+        .catch(err => console.log(err));
+    console.log(data);
+    var totalPages = Math.round(data.pageInfo.totalResults/10);
+    data.items.forEach(item => {
+        var i = document.getElementById("important");
+        var row = i.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+
+        var a = document.createElement("a");
+        var link = document.createTextNode("This is link");
+        a.appendChild(link); 
+        a.title = "This is Link"; 
+
+        a.href = "https://www.youtube.com/watch?v="+item.id.videoId;
+
+        var img = document.createElement("img");
+        img.src = item.snippet.thumbnails.high.url;
+
+        cell1.append(a);
+        cell2.innerHTML = item.snippet.title;
+        cell3.innerHTML = item.snippet.channelTitle; 
+        cell4.innerHTML = item.snippet.publishedAt;
+        cell5.appendChild(img);
+
+        document.getElementById("important").append(video);
+    });
+}
+
+
+// https://www.googleapis.com/youtube/v3/search?key=AIzaSyDsXJuqQWQiYb3BWrtxNVxaXaPmjhi7xAo
+// &type=video&part=snippet&maxResults=10&q=js
---
+++ script.js
@@ -0,0 +1,64 @@
+var text;
+console.log(text);
+const container =document.createElement("div");
+container.setAttribute('class',"container");
+
+const f = document.createElement("form");
+f.setAttribute('method', "post");
+f.setAttribute('action', "");
+
+const heading = document.createElement("h1");
+heading.innerHTML = "Search A Topic on YOUTUBE";
+
+const i = document.createElement("input");
+i.setAttribute('type', "text");
+i.setAttribute('name', "username");
+i.setAttribute('class',"input1")
+
+const s = document.createElement("input");
+s.setAttribute('type', "submit");
+s.setAttribute('value', "Submit");
+s.setAttribute('class',"submit-btn")
+
+const imp = document.createElement("table");
+imp.setAttribute('id',"important");
+var row = imp.insertRow(0);
+var cell1 = row.insertCell(0);
+cell1.setAttribute("id","tableHeading");
+var cell2 = row.insertCell(1);
+cell2.setAttribute("id","tableHeading");
+var cell3 = row.insertCell(2);
+cell3.setAttribute("id","tableHeading");
+var cell4 = row.insertCell(3);
+cell4.setAttribute("id","tableHeading");
+var cell5 = row.insertCell(4);
+cell5.setAttribute("id","tableHeading");
+
+cell1.innerHTML = "LINK"
+cell2.innerHTML = "TITLE"
+cell3.innerHTML = "CHANNEL NAME"
+cell4.innerHTML = "Publish At"
+cell5.innerHTML = "Thumbnail"
+
+f.appendChild(heading);
+f.appendChild(i);
+f.appendChild(s);
+// f.appendChild(imp);
+
+container.appendChild(f);
+
+document.body.appendChild(container);
+document.body.appendChild(imp);
+
+function onAddForm(e) {
+    e.preventDefault();
+    text = i.value;
+    console.log(text);
+    if(text === ""){
+        alert("Please Enter Topic Before Submitting.");
+        return false;
+    }
+    alert("didn't reach");
+    fetchingFromAPI();
+}
+f.addEventListener("submit", onAddForm);
